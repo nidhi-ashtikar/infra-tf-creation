c59cb273dae19b6e021b316a6420297b0a971d5e
Nishworl <ashtikarnidhi19@gmail.com>
2024-08-24 19:56:27 +0530

    Added config.tf

--- config.tf
+++ config.tf
@@ -1,8 +1,9 @@
 
 module "ec2" {
-  source = "git::https://github.com/nidhi-ashtikar/infra-tf-modules/main/ec2"
+  source = "git::https://github.com/nidhi-ashtikar/infra-tf-modules/main/ec2/"
 
   instance_type = "t2.micro"
 
 }
+
 
