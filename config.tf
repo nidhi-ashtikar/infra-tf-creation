68a16bbe770305408c2726a93878e60d6b1d239c
Nishworl <ashtikarnidhi19@gmail.com>
2024-08-24 20:52:44 +0530

    Adding security_group module

--- config.tf
+++ config.tf
@@ -7,6 +7,12 @@ module "ec2_instance" {
   source = "git::https://github.com/nidhi-ashtikar/infra-tf-modules.git//ec2"  # Adjust the path if your module is in a different directory
   instance_type = "t2.micro"
 
-
 }
 
+
+module "security_group" {
+  source = "https://github.com/nidhi-ashtikar/infra-tf-modules/blob/main/ec2/security_group.tf"
+  port = "22"
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
