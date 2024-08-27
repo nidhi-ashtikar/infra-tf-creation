808b3ccd4985d6aebbb4ad4acdc860c6a7274d19
Nishworl <ashtikarnidhi19@gmail.com>
2024-08-27 19:50:41 +0530

    new

--- vpc/config.tf
+++ vpc/config.tf
@@ -3,13 +3,8 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "git::https://github.com/shubham-thaware/terraform-module.git//vpc"
-  env =  var.env
-  vpc_name = var.vpc_name
-  vpc_cidr_block = var.vpc_cidr_block
-  additional_tags = var.additional_tags
-  public-subnet-cidr-1 = var.public-subnet-cidr-1
-  public-subnet-cidr-2 = var.public-subnet-cidr-2
-  private-subnet-cidr-1 = var.private-subnet-cidr-1
-  private-subnet-cidr-2 =  var.private-subnet-cidr-2
+  source = "git::https://github.com/nidhi-ashtikar/infra-tf-modules.git//vpc"
+  cidr_block_vpc = var.cidr_block_vpc
+  cidr_block_public_subnet1 = var.cidr_block_public_subnet1
+  cidr_block_public_subnet2 = var.cidr_block_public_subnet2
 }
--- vpc/variable.tf
+++ vpc/variable.tf
@@ -1,35 +1,12 @@
+variable "cidr_block_vpc" {
+  
+}
 variable "region" {
   
 }
-variable "vpc_name" {
+variable "cidr_block_public_subnet1" {
   
 }
-
-variable "vpc_cidr_block" {
+variable "cidr_block_public_subnet2" {
   
 }
-
-variable "additional_tags" {
-  
-}
-
-variable "public-subnet-cidr-1" {
-  
-}
-
-variable "public-subnet-cidr-2" {
-  
-}
-
-variable "private-subnet-cidr-1" {
-  
-}
-
-variable "private-subnet-cidr-2" {
-  
-}
-
-variable "env" {
-  
-}
-
